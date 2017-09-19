3154a693671845632c2454cf2341dbf0fed27147
TistaTech <mmetoff@tistatech.com>
2017-09-19 04:34:30 -0400

    Update

--- mail/src/test/java/net/serenity/bdd/junit/cucumber/RunnerClass.java
+++ mail/src/test/java/net/serenity/bdd/junit/cucumber/RunnerClass.java
@@ -8,7 +8,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
 		features = "src/test/resources/features/Compose", 
-		plugin = { "pretty","html:target/cucumber", "json:target/json/cucumber.json" },
+		plugin = {"pretty","html:target/cucumber", "json:target/json/cucumber.json"},
 		dryRun = false
 		)
 
